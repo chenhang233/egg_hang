757ae7561e0af4f4b276a3ee68c25ed844fc2b60
ChenHang <chenhang223333@163.com>
2022-08-06 10:03:55 +0800

    schedule

---
+++ app/schedule/update_cache.js
@@ -0,0 +1,22 @@
+const { Subscription } = require('egg')
+
+class UpdateCache extends Subscription {
+  // 通过 schedule 属性来设置定时任务的执行间隔等配置
+  static get schedule() {
+    return {
+      interval: '1m', // 间隔
+      type: 'worker', //ALL 指定所有的 worker 都需要执行
+      //   worker 类型：每台机器上只有一个 worker 会执行这个定时任务，每次执行定时任务的 worker 的选择是随机的
+      immediate: true,
+    }
+  }
+
+  // subscribe 是真正定时任务执行时被运行的函数
+  async subscribe() {
+    // const data = await this.ctx.service.sql.selectAll('logininfo')
+    // this.ctx.app.cache = data
+    console.log('定时任务test')
+  }
+}
+
+module.exports = UpdateCache
